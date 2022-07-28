22b17cfc153e0f62f2e22c7a44ab1638b681c03d
entriphy <1747088+entriphy@users.noreply.github.com>
2022-07-28 15:51:28 -0700

    DebugMenuLV: Add setting for enabling debug messages

--- DebugMenuLV/DebugMenuLV.cs
+++ DebugMenuLV/DebugMenuLV.cs
@@ -1,28 +1,48 @@
-﻿using System;
+using System;
 using BepInEx;
 using HarmonyLib;
+using App.Klonoa2;
+using UnityEngine;
+using BepInEx.Configuration;
 
-namespace DebugMenu
+namespace DebugMenuLV
 {
     [BepInPlugin("debug_menu_lv", "Klonoa 2 Debug Menu", "1.0.0")]
     [BepInProcess("Klonoa.exe")]
     public class DebugMenuPlugin : BaseUnityPlugin
     {
+        private ConfigEntry<bool> configEnableLog;
         private void Awake()
         {
-            Logger.LogInfo("Plugin debug_menu_lv is loaded!");
+            configEnableLog = Config.Bind<bool>("General", "EnableDebugLog", false, "Enables debug messages from the game. Logging.UnityLogListening must be enabled in the main BepInEx config.");
+            Log__Init.enableLog = configEnableLog.Value;
             var harmony = new Harmony("debug_menu_lv");
             harmony.PatchAll();
+            Logger.LogInfo("Plugin debug_menu_lv is loaded!");
         }
     }
 
-    [HarmonyPatch(typeof(App.Klonoa2.DebugSetting), "Awake")]
+    [HarmonyPatch(typeof(DebugSetting), "Awake")]
     public class DebugSetting__Awake
     {
         [HarmonyPrefix]
         public static void Prefix()
         {
-            App.Klonoa2.DebugSetting.instance.useDebugAction = true;
+            DebugSetting.instance.useDebugAction = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Log), "Init")]
+    public class Log__Init
+    {
+        public static bool enableLog = false;
+
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            Debug.unityLogger.logEnabled = enableLog;
+        }
+    }
         }
     }
 }
