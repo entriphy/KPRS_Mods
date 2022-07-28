7ca92ee92b8cb32f3582aad2c06504d0a0bbf803
entriphy <1747088+entriphy@users.noreply.github.com>
2022-07-28 00:22:28 -0700

    RestoreBShape: 1.1.0 -> 1.1.1

--- RestoreBShapeAnimations/RestoreBShapeAnimations.cs
+++ RestoreBShapeAnimations/RestoreBShapeAnimations.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 
 namespace RestoreBShapeAnimations
 {
-    [BepInPlugin("restore_bshape_animations", "Klonoa 2 - Restore Blend Shape Animations", "1.1.0")]
+    [BepInPlugin("restore_bshape_animations", "Klonoa 2 - Restore Blend Shape Animations", "1.1.1")]
     [BepInProcess("Klonoa.exe")]
     public class RestoreBShapeAnimationsPlugin : BaseUnityPlugin
     {
